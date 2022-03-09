344d0978fbc7d4469a5bf8538527f6aa125c54d5
Vishuddhi431 <81481451+Vishuddhi431@users.noreply.github.com>
2022-03-08 19:56:47 -0500

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,32 @@
+canvas = document.getElementById("my_canvas");
+color = "navy";
+ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(200, 200, 50, 0, 2*Math.PI);
+ctx.stroke();
+
+canvas.addEventListener("mousedown", myMouseDown);
+function myMouseDown(e){
+    color=document.getElementById("color").value;
+    console.log(color);
+    mouse_x = e.clientX-canvas.offsetLeft;
+    mouse_y = e.clientY-canvas.offsetTop;
+    console.log("x="+ mouse_x + "y=" + mouse_y);
+    circle(mouse_x, mouse_y);
+}
+
+function clearArea(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function circle(mouse_x, mouse_y){
+    ctx.beginPath();
+    ctx.strokeStyle=color;
+    ctx;linewidth=2;
+    ctx.arc(mouse_x, mouse_y, 50, 0, 2*Math.PI);
+    ctx.stroke();
+}
+
